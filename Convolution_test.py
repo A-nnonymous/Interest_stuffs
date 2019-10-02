7064c44d5778dce23b81dffbba2332883bb777cd
a617428699@163.com <pandatown1999>
2019-10-02 22:44:59 +0800

    Adding the discrete color channel function

--- Convolution_test.py
+++ Convolution_test.py
@@ -28,22 +28,34 @@ class CoreFrame:
                          [1 / 8,    1 / 8,  1 / 8]])
 
 
-def convolution(img, core, frame):
+def convolution(img, core, frame, discrete):
     height, width, channel = img.shape
     newimg = np.zeros((height + frame.height - 1, width + frame.width - 1, channel))
-    newimg[:, :, 0] = signal.convolve(img[:, :, 0], core)                  # concatenate the RGB channels with three 2D arrays
-    newimg[:, :, 1] = signal.convolve(img[:, :, 1], core)
-    newimg[:, :, 2] = signal.convolve(img[:, :, 2], core)                  # OR signal.fftconvolve(img, core)
+    new_R = signal.convolve(img[:, :, 0], core)       # OR signal.fftconvolve(img, core)
+    new_G = signal.convolve(img[:, :, 1], core)
+    new_B = signal.convolve(img[:, :, 2], core)
+    empchan = np.zeros((height + frame.height - 1, width + frame.width - 1))
+    if discrete:
+        imgB, imgG, imgR = newimg.copy(), newimg.copy(), newimg.copy()
+        imgR[:, :, 0], imgG[:, :, 0], imgB[:, :, 0] = new_R, empchan, empchan
+        imgR[:, :, 1], imgG[:, :, 1], imgB[:, :, 1] = empchan, new_G, empchan
+        imgR[:, :, 2], imgG[:, :, 2], imgB[:, :, 2] = empchan, empchan, new_B
+        newimg = np.concatenate((imgR,imgG,imgB), axis=1)
+    else:
+        newimg[:, :, 0] = new_R
+        newimg[:, :, 1] = new_G
+        newimg[:, :, 2] = new_B
+
     return newimg
 
 
-def main(name, save=False):
+def main(name, save=False, discrete=True):
     file_name = './resource/' + str(name) + '.png'
     img = mpimg.imread(file_name)
     frame = CoreFrame()
-    core = frame.itgrf()
+    core = frame.edge_detect()
     print('Convolution Core:', core)
-    newimg = convolution(img, core, frame)
+    newimg = convolution(img, core, frame,discrete)
     print(newimg.shape)
 
     plt.figure(figsize=(16, 9), dpi=120)
@@ -52,6 +64,7 @@ def main(name, save=False):
     plt.axis('off')
     if save:
         plt.savefig('./output/' + str(name) + '_processed' + '.png', dpi=120, transparent=True)
+        print("Picture saved as :"+'./output/' + str(name) + '_processed' + '.png')
     plt.show()
 
 
